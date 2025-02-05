4428b08f6a629569fd7a9beb5cfe830289774d5f
Ethan <ecala420@gmail.com>
2025-02-05 14:39:21 -0600

    added more comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//Ethan Cala Homework 2 for .Net Programming CPSC-23000
+
+// https://github.com/ethancala
+
+/*The application
+you'll create will calculate the values of polynomial functions of up to degree 3 over a domain. It
+will start with a heading. It will then ask you to enter the details of a polynomial, including its degree, its
+coefficients, and the min and max of the dom
+ain. Provided all the data was entered correctly, the application will
+tabulate the values over the domain. It will then ask the user if they want to enter the details for another
+polynomial. If they do, it will repeat the process*/
+
+
+using System;
 
 namespace Cala_NET_HW2
 {
